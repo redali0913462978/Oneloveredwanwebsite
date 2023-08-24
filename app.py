922ce487fcd927318619934e41858c1f7dd2e465
redali0913462978 <49614503+redali0913462978@users.noreply.github.com>
2023-08-24 17:02:22 +0000

    added dynamic data and API route
    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
+JOBE = [
+  {'id': 1,
+   'title':'Tunbolare',
+   'type of':'RHS AND SHS',
+   'size':'0.45 up to 6 mm'},
+  {'id':2,
+   'title':'Lamera',
+   'type of':'HOT ROLED AND COLOD ROLED',
+   'size':'0.45 up to 50 mm'},
+  {
+    'id':3,
+   'title':'TONDINO OR ROUND BAR',
+   'type of':'ABISINIA AND UKRAIN ROLED',
+   'size':'8mm up to 60 mm'
+  },
+  {
+    'id':4,
+   'title':'FLAT ROUND BAR',
+   'type of':'ABISINIA AND UKRAIN ROLED',
+   'size':'10 x 2 mm up to 80 x 6 mm'
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return "hello mother lovers !"
+  return render_template('home.html',  SHARP=JOBE,Company_name='Sharp Construction Material Retail')
 
-if __name__==("__main__"):
+
+@app.route("/api/SHARP")
+def list_SHARP():
+  return jsonify(JOBE)
+
+
+if __name__ == ("__main__"):
   app.run(host='0.0.0.0', debug=True)
